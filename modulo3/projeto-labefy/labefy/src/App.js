d542a748f3e0377f0eeab39c4dbda451502bb9bc
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-11 03:54:30 -0300

    Realizado o layout final ds página Home.

--- modulo3/projeto-labefy/labefy/src/App.js
+++ modulo3/projeto-labefy/labefy/src/App.js
@@ -3,12 +3,15 @@ import styled from "styled-components"
 import axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
+import Footer from "./components/layout/Footer";
 
 class App extends React.Component  {
   render () {
     return (
       <div>
+        <Navbar/>
          <Home/>
+         <Footer/>
       </div>
     )
   }
