263ff65a19b31dae4cad98d56455485435fff1a6
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-29 23:53:59 -0300

    Iniciado Projeto Labex, criado pages e wireframes.

---
+++ modulo4/projeto-labex/labex/src/pages/ApplicationFormPages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function ApplicationFormPages() {
+    return (
+        <div>
+            ApplicationFormPages
+        </div>
+    )
+}
+
+export default ApplicationFormPages
