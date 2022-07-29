689c88c5f0449161fa448764ce8b3a0f18b6f97c
renan-arimatea <renanmoraiz@gmail.com>
2022-07-29 00:46:32 -0300

    Realizado exercício aula Arquitetura de Software 2

--- labeflix/src/data/BaseDatabase.ts
+++ labeflix/src/data/BaseDatabase.ts
@@ -1,20 +1,17 @@
-import knex from 'knex'
-import dotenv from 'dotenv'
+import knex from "knex";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export abstract class BaseDatabase {
-
   protected static connection = knex({
-        client: "mysql",
-        connection: {
-          host: process.env.DATABASE_HOST,
-          port: 3306,
-          user: process.env.DATABASE_USER,
-          password: process.env.DATABASE_PASSWORD,
-          database: process.env.DATABASE_NAME,
-        }
-      })
-    }
-
-  
+    client: "mysql",
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
+  });
+}
